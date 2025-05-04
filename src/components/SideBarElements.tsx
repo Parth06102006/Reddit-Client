9a9341e7314959e9ce166788b86a3d007ef571dd
Parth <parth.6106garg@gmail.com>
2025-05-04 16:57:43 +0530

    Update The SideBar and Loading Element

---
+++ src/components/SideBarElements.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+type Props = {
+    title:string,
+    delete:(t:object)=>void
+}
+
+const SideBarElements = (props: Props) => {
+
+  return (
+    <div className='flex justify-between bg-slate-200/70 m-1 p-3 rounded hover:bg-white transition-all'>
+        <p className='font-bold text-[18px] text-gray-500'>{props.title}</p>
+        <div className='flex mr-3 gap-2 text-zinc-400 justify-center items-center'>
+            <i className="fa-solid fa-arrows-rotate hover:text-black"></i>
+            <div onClick={props.delete}><i className="fa-solid fa-trash hover:text-black"></i></div>
+        </div>    
+    </div>
+  )
+}
+
+export default SideBarElements
--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { SearchContext } from '../context/redditSearch'
 import { useContext } from 'react'
 import axios from 'axios'
@@ -14,20 +14,31 @@ const View = (props: Props) => {
     const {search} = useContext(SearchContext);
     const [reddits,setReddits] = useState([]);
     const [loading,setLoading] = useState(true);
+    let [items,setItems] = useState([]);
     console.log('Search',search)
     useEffect(()=>{
+      setLoading(true)
         const getReddit = async()=>
         {
+            items = JSON.parse(localStorage.getItem('items'))
             const response = await axios.get(`https://www.reddit.com/r/${search}.json`);
             console.log(response.data);
             setReddits(response.data.data.children)
+            setLoading(false);
+            setItems([...items,`/r/${search}`])
+            console.log(items)
         }
-        getReddit()
+        setTimeout(()=>{getReddit()},[2000])
+        return(()=>{
+          localStorage.setItem('items',JSON.stringify(items))
+          setLoading(false)})
     },[search])
     console.log(reddits)
   return (
     <>
-      {search &&  <div className='flex flex-col items-center'>
+      {loading ? <div className='w-[120px] h-[120px] rounded-full border-[16px] bg-linear-to-bl from-red-600 to-orange-300 animate-spin'>
+        <div className='relative bg-white w-[50px] h-[50px] rounded-full'></div>
+      </div> : <div className='flex flex-col items-center'>
       <div className='lg:mr-[550px] underline text-white animate-pulse'><h2 className='text-white font-bold text-3xl lg:text-center mb-5'>/r/{search}</h2></div>
       <div className=' w-fit top-0 inset-0 bg-zinc-600/20 overflow-y-scroll ml-[350px] py-7px-10 rounded-2xl h-[550px] p-3'>
             <div className='flex flex-wrap overflow-hidden gap-10'>
